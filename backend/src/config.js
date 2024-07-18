221092fd130ae2dccebfba825e1df9b9e1a746c8
= <dinaolmelak@gmail.com>
2024-07-18 13:18:04 -0500

    BugFix: adjusted Mongo_url

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,7 +4,10 @@ import process from 'process';
 
 
 dotenv.config();
-const MONGO_URI = process.env.REACT_APP_MONGO_URI;
+// mongodb+srv://dinaolmelak:<password>@cluster0.91fzntr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+console.log(process.env.REACT_APP_MONGO_URI);
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.REACT_APP_MONGO_URI}`;
 
 const connectDB = async () => {
     try {
