4abcc61b562b4b15f34de90dea75eefc1688d704
= <dinaolmelak@gmail.com>
2024-07-20 15:01:24 -0500

    Feature: Added rules for linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,24 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','filenames'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // Enforce camelCase for user-defined JSX components
+    'camelcase': 'error',
+    // Enforce PascalCase for user-defined JSX components
+    'react/jsx-pascal-case': 'error',
+    // Enforce lowercase filenames
+    'filenames/match-regex': [2, '[a-z][a-zA-Z0-9]+$', true],
+    // Enforce 2-space indentation
+    'indent': ['error', 2],
+    // Enforce single quotes for strings
+    'quotes': ['error', 'single'],
+    // Enforce semicolons at the end of statements
+    'semi': ['error', 'always'],
   },
 }
