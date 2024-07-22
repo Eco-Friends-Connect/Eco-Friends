98fc457f6d85bde5d2768361b6eb521afc8992bb
Donald Deal <Ddeal587@students.ccsnh.edu>
2024-07-22 19:47:03 -0400

    added custom font and more styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.scss';
+import treeImage from '../assets/tree.jpg';
+
 
 function Home() {
   const navigate = useNavigate();
@@ -8,13 +10,26 @@ function Home() {
   const handleSignupClick = () => {
     navigate('/signup');
   };
+  
+  
 
   return (
     <div>
-      <h2>Home Page</h2>
-      <button className={styles.signupButton} onClick={handleSignupClick}>
-        Sign up for help
-      </button>
+      <h1>Eco-Friends Connect</h1>
+      <div className={styles.treeimg}>
+      <img src={treeImage}  alt="Tree"></img>
+      <h1>To Better the Enviroment <br></br></h1> 
+      <h1>and Connect People</h1>
+      </div>
+      <div className={styles.buttoncontainer}>
+        
+          <button className={styles.signupButton} onClick={handleSignupClick}>
+            Sign up for help
+        </button>
+      
+      <button className={styles.calendarButton}>Calendar</button>
+      <button className={styles.howItWorksButton}>How it works</button>
+      </div>
     </div>
   );
 }
