94a84abc72d2756f2dfeacd13f8dc140cdf42db2
Donald Deal <Ddeal587@students.ccsnh.edu>
2024-07-28 19:07:55 -0400

    es lint compliance

--- src/component/signup-form/Signup.jsx
+++ src/component/signup-form/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './Signup.module.scss';
 import PropTypes from 'prop-types';
 
--- src/pages/SignupForm.jsx
+++ src/pages/SignupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Signup from '../component/signup-form/Signup';
 import DateCalendarServerRequest from '../component/SmallCalendar';
