966e1076efc5d72ffa0e170ae27d2ae7426fe9a8
Kendiya242 <zariaamber242@gmail.com>
2024-07-31 13:48:41 -0700

    login-form

---
+++ src/pages/OrgLogin.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "../component/login-form/Loginform.jsx";
+export default function OrganizationLogin(){
+    return(
+        <h1>
+            <LoginForm/>
+        </h1>
+
+
+    );
+}      
