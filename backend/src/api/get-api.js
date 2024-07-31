f3023cca30f16f60a6f36ae8eaba6639aeea6f4f
= <dinaolmelak@gmail.com>
2024-07-30 21:16:46 -0500

    Bugfix: optimized code

--- backend/src/api/get-api.js
+++ backend/src/api/get-api.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { getAuth } from 'firebase/auth';
 import Badge from '../../models/badge.js';
 import Membership from '../../models/membership.js';
-// import OrgBadge from '../../models/org_badge.js';
+import OrgBadge from '../../models/org_badge.js';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 const router = express.Router();
 
@@ -25,38 +25,40 @@ router.get('/badges', async (req, res) => {
     try {
         const badges = await Badge.find();
         const membership = await Membership.findOne({ accountId: auth.currentUser.uid });
+
         if (!membership) {
             return res.status(404).json({
                 status: 'error',
                 message: 'Membership not found',
             });
         }
-        // Create an array of promises for fetching the download URLs
-        const returnBadges = [];
+
         const badgePromises = badges.map(async (badge) => {
             const badgeWithLink = badge.toObject();
             if (badge.imgStorageRef) {
                 try {
                     const storageRef = ref(storage, badge.imgStorageRef);
-                    badge.imgUrl = await getDownloadURL(storageRef);
-                    // console.log('Fetched download URL for badge', badge._id);
-                    // console.log('Badge image URL:', badge.imgUrl);
-                    badgeWithLink.imgUrl = badge.imgUrl;
-                    returnBadges.push(badgeWithLink);
+                    badgeWithLink.imgUrl = await getDownloadURL(storageRef);
                 } catch (error) {
                     console.error('Error fetching download URL for badge', badge._id, error);
-                    badge.imgUrl = null; // or some default URL
+                    badgeWithLink.imgUrl = null; // or some default URL
                 }
-                await badge.save();
             }
+            return badgeWithLink;
         });
 
-        // Wait for all promises to complete
-        await Promise.all(badgePromises);
+        const badgeResults = await Promise.all(badgePromises);
+
+        const orgBadgeIds = await OrgBadge.find({ orgId: membership.orgId });
+        const orgBadgeIdSet = new Set(orgBadgeIds.map(orgBadge => orgBadge.badgeId.toString()));
+
+        const filteredBadges = badgeResults.filter(badge => 
+            orgBadgeIdSet.has(badge._id.toString())
+        );
 
         return res.json({
             status: 'success',
-            data: returnBadges,
+            data: filteredBadges,
         });
 
     } catch (error) {
