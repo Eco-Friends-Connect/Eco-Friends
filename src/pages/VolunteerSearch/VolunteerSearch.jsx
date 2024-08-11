e3a0f0cad937efc91a2100bc94bbcb6055256ac6
= <dinaolmelak@gmail.com>
2024-08-10 20:17:07 -0500

    Feature: populated volunteer search with events

--- src/pages/VolunteerSearch/VolunteerSearch.jsx
+++ src/pages/VolunteerSearch/VolunteerSearch.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import EventList from '../../component/event-list/Event-List';
 import SearchBar from '../../component/search-bar/search_bar';
 import styles from './VolunteerSearch.module.scss';
+import config from '../../config';
+import PopOut from '../../component/pop-out/pop-out';
 
 
 const VolunteerSearch = () => {
   const [signedUpEvent, setSignedUpEvent] = useState(null);
+  const [allEvents, setAllEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const events = [
     {
@@ -24,6 +29,35 @@ const VolunteerSearch = () => {
     },
    
   ];
+  async function fetchEvents() {
+    setLoading(true);
+    try {
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+      };
+      const fetchUrl = `${config.API_URL}/api/get/all-events`;
+      
+      const response = await fetch(fetchUrl, requestOptions).then((response) => response.json());
+      console.log('response', response);
+      let data = response.data;
+      data.forEach((element) => {
+        element.date = new Date(element.createdAt).toLocaleDateString();
+        element.location = "TBD";
+      });
+      setAllEvents(data);
+    } catch (error) {
+      console.error('Error fetching events', error);
+      setError('Error fetching events, please try again later\n' + error);
+    } finally {
+      setLoading(false);
+    }
+    
+  }
+
+  useEffect(() =>{
+    fetchEvents();
+  },[]);
 
   const handleSignUp = (event) => {
     setSignedUpEvent(event.title);
@@ -33,12 +67,22 @@ const VolunteerSearch = () => {
     <div>
       <h1 className={styles.anton}>Volunteer Opportunities</h1>
       <SearchBar />
-      <EventList events={events} onSignUp={handleSignUp} />
+      <EventList events={allEvents} onSignUp={handleSignUp} />
       {signedUpEvent && (
         <div style={{ marginTop: '20px', padding: '10px', border: '1px solid green', borderRadius: '5px', backgroundColor: '#e0f7fa' }}>
           <h2>Thank you for signing up!</h2>
           <p>You have signed up for: {signedUpEvent}</p>
         </div>
+      )}
+      {loading && (
+        <PopOut isOpened={loading} popOutType="info" onClose={() => {}}>
+          <p>Loading events...</p>
+        </PopOut>
+      )}
+      {error && (
+        <PopOut isOpened={error === null ? false : true} popOutType="error" onClose={() => {setError(null)}}>
+          <p>{error}</p>
+        </PopOut>
       )}
     </div>
   );
