41fba0b7bbe402c44c1d919bc79fdd7ff8c2458d
Donald Deal <Ddeal587@students.ccsnh.edu>
2024-08-13 06:51:28 -0400

    customized buttons and added functionality

--- src/component/hidden-buttons/HiddenButtons.jsx
+++ src/component/hidden-buttons/HiddenButtons.jsx
@@ -1,10 +1,32 @@
 import styles from './HiddenButtons.module.scss';
+import { useNavigate } from "react-router-dom";
+import React from 'react';
 
 export default function HiddenButtons() {
+    const navigate = useNavigate();
+
+    const VolunteerButtonClick = () => {
+        navigate("/volunteersearch");
+    };
+
+    const UserButtonClick = () => {
+        navigate("/userpage");
+    };
+
+
     return(
         <div className={styles.buttoncontainer}>
-            <button className={styles.responsivebutton}>Button 1</button>
-            <button className={styles.responsivebutton}>Button 2</button>
+            <button
+                onClick={VolunteerButtonClick}
+                className={styles.responsivebutton}>
+                VolunteerSignup
+            </button>
+            
+            <button
+                onClick={UserButtonClick}
+                className={styles.responsivebutton}
+                >UserPage
+            </button>
         </div>
     );
 }
