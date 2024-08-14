3e3ea700631fa73697d195b630353cd8f8e862a7
= <dinaolmelak@gmail.com>
2024-08-13 21:20:10 -0500

    Feature: Added Docker & installed npm packages

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-import express, { application } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config.js';
@@ -56,7 +56,7 @@ app.use('/api/get', getApi);
 
 // environment variables
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
