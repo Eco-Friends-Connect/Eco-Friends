b3c070f8c6580773708b7327d71779c51b3162bd
= <dinaolmelak@gmail.com>
2024-08-15 23:42:24 -0500

    Bugfix:change reverted

--- src/config.js
+++ src/config.js
@@ -1,6 +1,3 @@
-import { config as dotenvConfig } from "dotenv";
-dotenvConfig();
-
 const config = {
     // The following is the URL to the API for production
     // API_URL: 'https://eco-friends-5tm5cvcguq-uc.a.run.app',
