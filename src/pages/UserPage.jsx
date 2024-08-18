a6d17b811fa04ffb0d499862fcc428b36675d685
Donald Deal <Ddeal587@students.ccsnh.edu>
2024-08-17 21:07:27 -0400

    added navbar with logout to userpage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,5 +1,6 @@
 import React,{useContext} from 'react';
 import UserComponent from '../component/user/user';
+import NavBar from '../component/navbar/Navbar';
 import AuthContext from '../component/auth-context';
 import Lottie from 'lottie-react';
 import groovyWalkAnimation from '../assets/lotties/groovyWalk.json';
@@ -9,6 +10,7 @@ export default function UserPage() {
   
   return (
     <>
+      <NavBar />
       <UserComponent />
       {/* <Lottie animationData={groovyWalkAnimation} loop={true} /> */}
     </>
